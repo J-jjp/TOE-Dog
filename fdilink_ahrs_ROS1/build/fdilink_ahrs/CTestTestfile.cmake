b4b5a8337837ef5544f6bee10c1523a612e02b17
pengunix <hoshimamei@outlook.com>
2025-01-17 00:21:13 +0800

    first

--- fdilink_ahrs_ROS1/build/fdilink_ahrs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/toe/TOE-Dog/fdilink_ahrs_ROS1/src/fdilink_ahrs
-# Build directory: /home/toe/TOE-Dog/fdilink_ahrs_ROS1/build/fdilink_ahrs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
