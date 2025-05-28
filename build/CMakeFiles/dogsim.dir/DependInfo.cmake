59446a64d94e41412f38db589077a4697df2509f
J-jjp <2053930980@qq.com>
2025-05-28 22:08:58 +0800

    first

---
+++ build/CMakeFiles/dogsim.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/dog/TOE-Dog/main.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/main.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/FSM/FSM.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/FSM/FSM.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/FSM/FSMState.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/FSM/FSMState.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/FSM/State_FixedStand.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/FSM/State_FixedStand.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/FSM/State_FreeStand.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/FSM/State_FreeStand.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/FSM/State_Passive.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/FSM/State_Passive.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/FSM/State_Rl.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/FSM/State_Rl.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/common/unitreeLeg.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/common/unitreeLeg.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/common/unitreeRobot.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/common/unitreeRobot.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/control/ControlFrame.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/control/ControlFrame.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/control/leg_control.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/control/leg_control.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/control/rl_Inference.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/control/rl_Inference.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/interface/IOMujoco.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/interface/IOMujoco.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/interface/KeyBoard.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/interface/KeyBoard.cpp.o"
+  "/home/ubuntu/dog/TOE-Dog/src/interface/WirelessHandle.cpp" "/home/ubuntu/dog/TOE-Dog/build/CMakeFiles/dogsim.dir/src/interface/WirelessHandle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILE_WITH_REAL_ROBOT"
+  "CONTEST_TYPE_SPEED"
+  "ROBOT_TYPE_T2"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  ".././mujoco-3.2.5/simulate"
+  ".././mujoco-3.2.5/include"
+  ".././imgui"
+  ".././imgui/backends"
+  "../include/interface"
+  "../include/message"
+  "../include/FSM"
+  "../include/communication"
+  "../MNN/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
